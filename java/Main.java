b270eab7a3c6aa499c94a89b9ed675e47c322c54
Debojyoti Tantra <tantradebojyoti@gmail.com>
2025-05-13 09:10:45 +0000

    Initial commit

---
+++ java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static int add(int a, int b) {
+        int summ = a + b;
+        return summ;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(add(2, 5));
+    }
+}
